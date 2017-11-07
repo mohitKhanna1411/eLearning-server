370dcb6a82e57fa05def6b6ad8895c482da513e3
Mohit Khanna <khanna.mohit94@gmail.com>
2017-11-07 16:32:34 +0530

    result schema created

---
+++ models/result.js
@@ -0,0 +1,34 @@
+//var fs = require('fs');
+var mongoose = require('mongoose');  
+
+var resultSchema = mongoose.Schema({
+            standard:{ 
+                  type: String
+            },
+            section:{ 
+                  type: String
+            },
+            subject:{ 
+                  type: String
+            },
+            
+            student_id:{
+                  type : String
+            }
+            marks : {
+                  type : String
+            }
+            recommendation : [
+                  {
+                        lesson_id : {type : String}
+                  }
+            ] 
+});
+
+resultSchema.index({ standard: 1, section: 1, subject: 1 , student_id : 1 }, { unique: true });
+// var Test = db.mode("Test", testSchema );
+
+var Result = mongoose.model('Result', resultSchema);  
+module.exports = Result;
+
+
