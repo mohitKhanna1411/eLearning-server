5836b8168c471474771222e64ab2cb4859203e8a
Mohit Khanna <khanna.mohit94@gmail.com>
2017-12-17 17:10:42 +0530

    -

--- models/class.js
+++ models/class.js
@@ -12,41 +12,39 @@ var classSchema = mongoose.Schema({
             type: String
       },
       
-      students:[
-      {
-            Student_ID : {type : String },
-            _id : false
-      }
-      ],
-      lessons : [
-      {
+      students:[{
+            _id : false,
+            Student_ID : {type : String }
+            
+      }],
+      lessons : [{
+            _id : false,
             Title :  {type : String},
             Content : {type : String},
             Ref_Link : {type : String},
-            Ref_Video : {type : String},
-            _id : false
-      }
-      ] ,
-      error_codes: [
-      {
-            error_code : {type : String},
-            lesson_title : {type : String},
-            _id : false
-      }
-      ] ,
-      assesment : [
-      {
+            Ref_Video : {type : String}
+      }],
+      error_codes: [{
             _id : false,
-            question : {type : String},
-            options : [{
-                  answerText : {type : String},
-                  correct : {type : Boolean},
-                  error_lesson_title : {type : String},
-                  _id : false
-            }]
-
-      }
-      ] 
+            error_code : {type : String},
+            lesson_title : {type : String}
+      }],
+      assesments :[{
+            _id : false,
+            assesment_name : {type : String},
+            lesson_title : {type : String},
+            assesment : [{
+                        _id : false,
+                        question : {type : String},
+                        options : [{
+                              _id : false,
+                              answerText : {type : String},
+                              correct : {type : Boolean},
+                              error_lesson_title : {type : String}
+                        }]
+                  }]
+      }]
+                   
 });
 
 classSchema.index({ standard: 1, section: 1, subject: 1}, { unique: true });
