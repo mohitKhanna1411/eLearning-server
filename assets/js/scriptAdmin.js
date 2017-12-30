b2e79eb361aee2019fb9b11779a4bf7b79d5d689
Adityaa Chaubey <adityaa.chaubey@gmail.com>
2017-12-31 02:11:04 +0530

    to be tested

--- assets/js/scriptAdmin.js
+++ assets/js/scriptAdmin.js
@@ -167,7 +167,7 @@ myApp.factory('readErrorCodeData',['$http', function($http) {
       var lines = [];
       var json = {};
 
-      for (var i = 0; i < record.length; i++) {
+      for (var i = 1; i < record.length; i++) {
         var data = record[i].split(',');
         if (data.length == headers.length) {
           var tarr = [];
